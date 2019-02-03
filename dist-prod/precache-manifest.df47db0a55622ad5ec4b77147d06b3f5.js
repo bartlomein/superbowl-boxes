337401b262985f7e0478989ebc836e24b4ef0e98
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2019-02-03 13:33:24 -0500

    prod folder

---
+++ dist-prod/precache-manifest.df47db0a55622ad5ec4b77147d06b3f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0043dbc1ccf7fef90459",
+    "url": "/static/js/main.0043dbc1.chunk.js"
+  },
+  {
+    "revision": "965b0478ff71f2e13d3f",
+    "url": "/static/js/1.965b0478.chunk.js"
+  },
+  {
+    "revision": "0043dbc1ccf7fef90459",
+    "url": "/static/css/main.8cfcd905.chunk.css"
+  },
+  {
+    "revision": "d6816ebcd5b71ff95dee338a5f46afb3",
+    "url": "/index.html"
+  }
+];
